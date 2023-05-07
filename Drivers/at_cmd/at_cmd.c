6af78bbc05ac6fb02361e2d310b575c526c6e360
ad56583964 <1176949486@qq.com>
2023-05-07 22:23:18 +0800

    FIX: AT_request重试没有考虑重新开启UART

--- Drivers/at_cmd/at_cmd.c
+++ Drivers/at_cmd/at_cmd.c
@@ -42,8 +42,6 @@ AT_Status_t AT_request_send_pack (AT_Request_Set_t* pack){
 }
 
 AT_Status_t AT_request (AT_Request_Set_t* pack,AT_Receive_Read_t* get_pack){
-	clear_semaphore();
-	start_receive();
 
 	AT_request_pack.addr[0] = pack->addr >> 8;
 	AT_request_pack.addr[1] = pack->addr&0xff;
@@ -53,6 +51,8 @@ AT_Status_t AT_request (AT_Request_Set_t* pack,AT_Receive_Read_t* get_pack){
 
 	char _state = -1;
 	while(_state != osOK){
+		clear_semaphore();
+		start_receive();
 		AT_Send((uint8_t*)&AT_request_pack, 15);
 		_state = wait_receive(200);
 		_is_send_ok();
